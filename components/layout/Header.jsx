1bce4fc89c25d7cdf93a0780adfa9760c051f3b2
MUKEBUDAVID <davidmukebu@gmail.com>
2023-10-28 16:28:36 +0100

    premier commit

---
+++ components/layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+    <p>Mukebu</p>
+    
+    
+    
+    
+    <ul>
+ <li></li>
+ <li></li>
+ <li></li>
+ <li></li>          
+    </ul>
+    </header>
+  )
+}
+
+export default Header
---
+++ pages/api/hello.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json(
+    { name: 'John Doe',
+    name2: 'John Doe',
+    name3: 'John Doe',
+    name4: 'John Doe',
+    name5: 'John Doe',
+    name6: 'John Doe',
+    name7: 'John Doe',
+    name8: 'John Doe'
+   }
+    )
+}
