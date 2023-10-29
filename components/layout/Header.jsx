1df497eebff205f0ac528644c8a12053f795f5aa
MUKEBUDAVID <davidmukebu@gmail.com>
2023-10-29 17:26:58 +0100

    fin du header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
+
 
 function Header() {
   return (
     <header>
-    <p>Mukebu</p>
-    
-    
-    
-    
-    <ul>
- <li></li>
- <li></li>
- <li></li>
- <li></li>          
-    </ul>
+      <p className="logo">Mukebu</p>
+
+      <div className="right">
+        <nav >
+          <a href="#">Home</a>
+          <a href="#">Skills</a>
+          <a href="#">Projects</a>
+        </nav>
+
+        <section className="reseaux">
+          <a href="#">
+            <img src="/nav-icon1.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/nav-icon2.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/nav-icon3.svg" alt="" />
+          </a>
+        </section>
+
+        <button type="button">Let's Connect</button>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- components/layout/Main.jsx
+++ components/layout/Main.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Main() {
   return (
-    <div>Main</div>
+    <main >
+      <section className="bannier">
+        <div className="welcom">
+          
+           <div className="presentation">
+            <div>Welcome to my Portfolio</div>
+            <div>Hi! I'm David Mukebu i am web Developper</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis ad quae harum deserunt iusto necessitatibus autem minus adipisci quo. Molestias 
+              error nostrum obcaecati alias distinctio itaque reprehenderit soluta a!</p>
+           </div>
+
+         
+            <img src="/header-img.svg" alt="svg" />
+           
+
+        </div>
+
+      </section>
+
+
+    </main>
   )
 }
 
